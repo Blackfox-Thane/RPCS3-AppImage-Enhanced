8c93532f960c8e3d6b7672f8072514dc581e92cc
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-08-02 20:46:20 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,50 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-sudo pacman -Syu --noconfirm minizip \
-							 qt6-base \
-							 kvantum \
-							 qt6ct \
-							 lxqt-qtplugin \
+pacman -Syu --noconfirm glew \
+  glu \
+  openal \
+  qt6-base \
+  qt6-declarative \
+  qt6-multimedia \
+  qt6-base \
+  pugixml \
+  llvm \
+  opencv \
+  ffmpeg \
+  sdl3 \
+  sndio \
+  jack2 \
+  vulkan-validation-layers
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 ./get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-# sudo ./make-aur-package doomrunner
+make-aur-package rpcs3-git
 
 # If the application needs to be manually built that has to be done down here
-echo "Getting app..."
-echo "---------------------------------------------------------------"
-wget https://github.com/Youda008/DoomRunner/releases/download/v1.9.2/DoomRunner-1.9.2-Linux-x86_64-dynamic_exe.zip
-unzip ./DoomRunner-1.9.2-Linux-x86_64-dynamic_exe.zip -d ./AppDir/bin
+
+# echo "Building RPCS3..."
+# echo "---------------------------------------------------------------"
+# git clone https://github.com/RPCS3/rpcs3.git
+# cd rpcs3
+# git submodule sync
+
+# if you also have to make nightly releases check for DEVEL_RELEASE = 1
+#
+# if [ "${DEVEL_RELEASE-}" = 1 ]; then
+# 	nightly build steps
+# else
+# 	regular build steps
+# fi
+
+# mkdir ./build
+# cd ./
+# git submodule update --init --recursive
+# cmake .. \
+#   -Bbuild \
+#   -G Ninja \
+# cmake --build build
+

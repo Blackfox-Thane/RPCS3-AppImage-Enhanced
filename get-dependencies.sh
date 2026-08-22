8d9fc85417bede81e102167e58321b6db198e8bf
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-08-22 19:26:03 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -27,7 +27,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package rpcs3-git
+make-aur-package --chaotic-aur rpcs3-git
 
 # If the application needs to be manually built that has to be done down here
 
